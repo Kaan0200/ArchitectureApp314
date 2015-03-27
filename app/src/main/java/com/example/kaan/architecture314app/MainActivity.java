6adda02575c3ce3c8478a3345516a1f52c34253c
Kaan Atesoglu <aka43@case.edu>
2015-03-26 21:40:09 -0400

    Initials of a project with some code and an error

---
+++ app/src/main/java/com/example/kaan/architecture314app/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.kaan.architecture314app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private String outputTextLabel = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Kaan created method that does the calculation using the values from all the fields
+    // on screen.  It does the calculation then updates the output text, it does not return the
+    // value, it is called when the button is pressed.
+    public void calculate(){
+        outputTextLabel = "150Test";
+    }
+}
